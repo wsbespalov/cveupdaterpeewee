f5a19a30c83df9437b78407679368a619ab6b6a3
Konstantin <wetarmor@gmail.com>
2018-04-19 17:50:16 +0300

    added new fields for data property

--- src/updater/model_cve.py
+++ src/updater/model_cve.py
@@ -201,16 +201,48 @@ class CVE_VULNERS(peewee.Model):
         cves_data = {}
         cves_data["id"] = self.id
         cves_data["cve"] = self.item
-        cves_data["data_type"] = self.data_type
         cves_data["data_format"] = self.data_format
+        cves_data["data_type"] = self.data_type
         cves_data["data_version"] = self.data_version
-        cves_data["references"] = self.references
-        cves_data["published"] = self.published
+        cves_data["description"] = self.description
         cves_data["last_modified"] = self.last_modified
+        cves_data["published"] = self.published
+        cves_data["references"] = self.references
+        cves_data["vendors"] = self.convert_list_data_from_json(self.vendors)
         cves_data["cpe22"] = self.cpe22
         cves_data["cpe23"] = self.cpe23
         cves_data["cwe"] = self.cwe
-        cves_data["vendors"] = self.convert_list_data_from_json(self.vendors)
+        cves_data["cvssv2_access_complexity"] = self.cvssv2_access_complexity
+        cves_data["cvssv2_access_vector"] = self.cvssv2_access_vector
+        cves_data["cvssv2_authentication"] = self.cvssv2_authentication
+        cves_data["cvssv2_availability_impact"] = self.cvssv2_availability_impact
+        cves_data["cvssv2_base_score"] = self.cvssv2_base_score
+        cves_data["cvssv2_confidentiality_impact"] = self.cvssv2_confidentiality_impact
+        cves_data["cvssv2_exploitability_score"] = self.cvssv2_exploitability_score
+        cves_data["cvssv2_impact_score"] = self.cvssv2_impact_score
+        cves_data["cvssv2_integrity_impact"] = self.cvssv2_integrity_impact
+        cves_data["cvssv2_obtain_all_privilege"] = self.cvssv2_obtain_all_privilege
+        cves_data["cvssv2_obtain_other_privilege"] = self.cvssv2_obtain_other_privilege
+        cves_data["cvssv2_obtain_user_privilege"] = self.cvssv2_obtain_user_privilege
+        cves_data["cvssv2_severity"] = self.cvssv2_severity
+        cves_data["cvssv2_user_interaction_required"] = self.cvssv2_user_interaction_required
+        cves_data["cvssv2_vector_string"] = self.cvssv2_vector_string
+        cves_data["cvssv2_version"] = self.cvssv2_version
+        cves_data["cvssv3_attack_complexity"] = self.cvssv3_attack_complexity
+        cves_data["cvssv3_attack_vector"] = self.cvssv3_attack_vector
+        cves_data["cvssv3_availability_impact"] = self.cvssv3_availability_impact
+        cves_data["cvssv3_base_score"] = self.cvssv3_base_score
+        cves_data["cvssv3_base_severity"] = self.cvssv3_base_severity
+        cves_data["cvssv3_confidentiality_impact"] = self.cvssv3_confidentiality_impact
+        cves_data["cvssv3_exploitability_score"] = self.cvssv3_exploitability_score
+        cves_data["cvssv3_impact_score"] = self.cvssv3_impact_score
+        cves_data["cvssv3_integrity_impact"] = self.cvssv3_integrity_impact
+        cves_data["cvssv3_privileges_required"] = self.cvssv3_privileges_required
+        cves_data["cvssv3_scope"] = self.cvssv3_scope
+        cves_data["cvssv3_user_interaction"] = self.cvssv3_user_interaction
+        cves_data["cvssv3_vector_string"] = self.cvssv3_vector_string
+        cves_data["cvssv3_version"] = self.cvssv3_version
+
         return cves_data
 
     @staticmethod
